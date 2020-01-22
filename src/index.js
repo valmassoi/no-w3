94a0f061b4c1a54e869dc26bf7ffab344902c7fc
Robert Valmassoi <valmassoi@protonmail.com>
2020-01-22 12:29:59 -0500

    init lint

--- src/index.js
+++ src/index.js
@@ -2,23 +2,24 @@ import { name, version } from '../package.json';
 
 const removeFromDDG = () => {
   const DDGw3Els = document.querySelectorAll("div[data-hostname='www.w3schools.com']"); // :not([style*="display:none"])
-  DDGw3Els.forEach(w3El => {
-    const childATag = w3El.querySelector('a');
-    const href = childATag.getAttribute("href");
+  DDGw3Els.forEach((el) => {
+    const parent = el;
+    const childATag = parent.querySelector('a');
+    const href = childATag.getAttribute('href');
     console.info(`${name}: removing ${href}`);
-    w3El.style.display = 'none';
+    parent.style.display = 'none';
   });
-}
+};
 
 const removeFromGoogle = () => {
   // CREDIT: selector from https://github.com/GMaiolo/remove-w3schools/blob/master/scripts/remover.js#L6
   const googleW3Els = document.querySelectorAll('div.g:not([style*="display:none"]):not([style*="display: none"]) .r > a[href*="www.w3schools.com"]');
-  googleW3Els.forEach(el => {
+  googleW3Els.forEach((el) => {
     const parent = el.closest('#rso div.g');
     if (!parent) return console.warn(`${name}: Google selectors need to be updated.`);
     parent.style.display = 'none';
-  })
-}
+  });
+};
 
 const removeW3Elements = (domain) => ({
   'www.duckduckgo.com': removeFromDDG(),
@@ -31,5 +32,4 @@ const app = () => {
     removeW3Elements(window.location.hostname);
   };
 };
-} 
 app();
